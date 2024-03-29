2804e4ef497cc67ab0aadc3edc081948e4c8fd5c
Joel Dacuma <joeldacuma1@gmail.com>
2024-03-29 12:52:57 +0800

    update scroller area for page

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -31,8 +31,8 @@ const Settings = () => {
   }, [userContent, surveyInfo]);
 
   return (
-    <div className="flex justify-center px-12 py-6">
-      <ScrollArea className="h-[84vh] w-[60vw] rounded">
+    <div className="flex justify-center px-8 py-6">
+      <ScrollArea className="h-[84vh] hidden lg:block w-[60vw] rounded">
         <div className="bg-white rounded-3xl lg:px-12 px-8 py-8">
           <h1 className="text-3xl font-bold">Profile Information</h1>
           <hr className="my-10" />
@@ -75,6 +75,49 @@ const Settings = () => {
           </div>
         </div>
       </ScrollArea>
+      <div className="block lg:hidden">
+        <div className="bg-white rounded-3xl lg:px-12 px-8 py-8">
+          <h1 className="text-3xl font-bold">Profile Information</h1>
+          <hr className="my-10" />
+          <div className="grid md:grid-cols-1 lg:grid-cols-1 gap-10">
+            {surveyInfo?.error || !data ? (
+              <div>
+                <Button onClick={handleEditUserSurvey}>Edit Info</Button>
+              </div>
+            ) : (
+              <div className="space-y-4">
+                <Card className="w-full bg-white border rounded-xl text-gray-800 space-y-2">
+                  <div className="flex justify-between items-center p-4">
+                    <span className="text-xl font-bold">Business Details</span>
+                    <Pencil2Icon
+                      onClick={handleEditUserSurvey}
+                      className="w-6 h-6 hover:cursor-pointer"
+                    />
+                  </div>
+                  <CardHeader className="flex justify-between">
+                    {userContent?.userSurveyContent?.data?.Questions.map(
+                      (survey: any, index: number) => (
+                        <div key={index}>
+                          <CardTitle className="py-4">
+                            {survey.Question}
+                          </CardTitle>
+                          <span className="text-gray-400 text-md">
+                            {
+                              surveyInfo?.membersContentDetails[
+                                USER_SURVEY_MAPPING[index]
+                              ]
+                            }
+                          </span>
+                        </div>
+                      )
+                    )}
+                  </CardHeader>
+                </Card>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
